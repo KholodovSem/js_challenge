89636b09a68573aa8a02f4ca5181228073487c7c
Sem Kholodov <kholodovsem@gmail.com>
2022-12-01 13:23:45 +0200

    e

---
+++ src/helpers/index.js
@@ -0,0 +1,22 @@
+import cherio from 'cherio';
+import { getPageContent } from "./puppeteer.js";
+
+async function getLinks(URL, deep) {
+  if (!getLinks.links) {
+    getLinks.links = new Set();
+    getLinks.baseURL = URL;
+  }
+  const content = await getPageContent(URL);
+
+  const $ = cherio.load(content);
+  $('a').each((i, element) => {
+    if ($(element).attr('href') && $(element).attr('href').startsWith('/')) {
+      getLinks.links.add(getLinks.baseURL + $(element).attr('href'));
+    }
+  });
+
+  console.log(getLinks.links);
+}
+
+getLinks("https://madappgang.com", 3);
+
